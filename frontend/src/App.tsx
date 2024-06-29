da1401fe8381943278d914a3c6f7d6497c6adbbc
Jacob <jacobschwantes@outlook.com>
2024-06-29 12:51:01 -0500

    test 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 	const { state } = useRoomContext();
 	return (
     <main className="flex flex-1 flex-col items-center">
-      test
+      test2
 			{state.code ? <Canvas /> : <JoinRoomCard />}
 		</main>
 	);
