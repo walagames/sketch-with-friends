87aeb60aef5933299c67e44725222608c20442ee
Jacob <jacobschwantes@outlook.com>
2024-07-13 18:50:20 -0500

    Test

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,10 +6,6 @@ import { RoomForm } from "@/components/room/room-form";
 export default function Home() {
 	const { room } = useRoomContext();
 
-	// TODO: useMemo on room.status to render form or room view
-	// TODO: use roomRole to render player or host view
-	// TODO: use gameRole to render drawing or guessing view
-
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between">
 			{room.code ? (
