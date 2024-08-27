bc126d6cc241b564cd5d4b996d384307ce0b51cc
Jacob <jacobschwantes@outlook.com>
2024-08-27 17:15:08 -0500

    game phase animations

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,10 +4,12 @@ import Canvas from "@/components/canvas/canvas";
 import { RoomUninitialized } from "@/components/room/room-uninitialized";
 import { RoomStatus } from "@/types/room";
 import { Button } from "@/components/ui/button";
-import Waiting from "@/components/room/waiting";
-
+import Waiting from "@/components/room/room-waiting";
+import { RoomLayout } from "@/components/room/room-layout";
+import { Playing } from "@/components/room/room-playing";
+import { AnimatePresence } from "framer-motion";
 const roomViews = {
-	[RoomStatus.PLAYING]: Canvas,
+	[RoomStatus.PLAYING]: Playing,
 	[RoomStatus.WAITING]: Waiting,
 	[RoomStatus.UNINITIALIZED]: RoomUninitialized,
 };
@@ -18,8 +20,16 @@ export default function Home() {
 	const RoomView = roomViews[room.status as keyof typeof roomViews];
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<RoomView />
+		<main className="flex min-h-screen flex-col items-center justify-between relative">
+			<RoomLayout>
+				<AnimatePresence mode="popLayout">
+					{room.status === RoomStatus.PLAYING && <Playing key="playing" />}
+					{room.status === RoomStatus.UNINITIALIZED && (
+						<RoomUninitialized key="uninitialized" />
+					)}
+					{room.status === RoomStatus.WAITING && <Waiting key="waiting" />}
+				</AnimatePresence>
+			</RoomLayout>
 		</main>
 	);
 }
