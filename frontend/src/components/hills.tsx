e49ff9508ad5d0328e283af9b721adbf77b10e33
Jacob <jacobschwantes@outlook.com>
2024-10-11 21:29:50 -0500

    add hill graphics

---
+++ frontend/src/components/hills.tsx
@@ -0,0 +1,12 @@
+export function Hills() {
+	return (
+		<div>
+			<div className="absolute -bottom-24 left-0 w-full">
+				<img src="/hill-right.svg" alt="footer" className="w-full" />
+			</div>
+			<div className="absolute -bottom-20 translate-y-[1px] left-0 w-full">
+				<img src="/hill-left.svg" alt="footer" className="w-full" />
+			</div>
+		</div>
+	);
+}
