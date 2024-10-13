9f665b454533c71d337c298fdd9fe9836e81f2bf
Jacob <jacobschwantes@outlook.com>
2024-10-13 14:40:08 -0500

    room code schema validation

--- frontend/src/components/views/join-room/enter-code-view.tsx
+++ frontend/src/components/views/join-room/enter-code-view.tsx
@@ -1,31 +1,97 @@
-import { Input } from "@/components/ui/input";
-import { motion } from "framer-motion";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useDispatch } from "react-redux";
 import { Hills } from "@/components/hills";
-import { useDirectionAnimation } from "@/App";
 import { RaisedButton } from "@/components/raised-button";
 import { enterRoomCode } from "@/state/features/client";
-import { useState } from "react";
+import { useEffect } from "react";
 import { ArrowRightIcon } from "lucide-react";
 import { clearQueryParams } from "@/lib/params";
-export function EnterCodeView() {
-	const animationProps = useDirectionAnimation();
-	const dispatch = useDispatch();
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
+const CodeFormSchema = z.object({
+	roomCode: z
+		.string()
+		.length(6, {
+			message: "Room code must be 6 characters long.",
+		})
+		.regex(/^[A-Za-z]+$/, {
+			message: "Room code must contain only letters.",
+		}),
+});
+
+function CodeForm() {
+	const dispatch = useDispatch();
 	const searchParams = new URLSearchParams(location.search);
 	const roomCodeParam = searchParams.get("room");
 
-	const [roomCode, setRoomCode] = useState(roomCodeParam ?? "");
+	const form = useForm<z.infer<typeof CodeFormSchema>>({
+		resolver: zodResolver(CodeFormSchema),
+		defaultValues: {
+			roomCode: roomCodeParam ?? "",
+		},
+	});
 
-	function onSubmit(code: string) {
-		dispatch(enterRoomCode(code));
+	useEffect(() => {
+		form.setValue("roomCode", roomCodeParam ?? "");
+	}, [roomCodeParam, form]);
+
+	function onSubmit(data: z.infer<typeof CodeFormSchema>) {
+		dispatch(enterRoomCode(data.roomCode));
 	}
 
 	return (
-		<motion.div
-			{...animationProps}
-			className="w-full h-full absolute inset-0 flex flex-col items-center justify-center gap-8"
-		>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+				<FormField
+					control={form.control}
+					name="roomCode"
+					render={({ field }) => (
+						<FormItem>
+							<FormControl>
+								<div className="relative">
+									<div className="flex items-center gap-3 bg-secondary-foreground rounded-lg">
+										<Input
+											{...field}
+											autoComplete="off"
+											placeholder="Room code"
+											className="font-bold text-xl text-zinc-400 placeholder:text-zinc-400 bg-background rounded-lg h-14 px-4 py-3.5 w-64 -translate-y-1.5 translate-x-1.5"
+										/>
+									</div>
+									<div className="absolute -right-14 top-2">
+										<RaisedButton
+											type="submit"
+											shift={false}
+											variant="action"
+											size="icon"
+										>
+											<ArrowRightIcon className="w-6 h-6" />
+										</RaisedButton>
+									</div>
+								</div>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+			</form>
+		</Form>
+	);
+}
+
+export function EnterCodeView() {
+	const dispatch = useDispatch();
+
+	return (
+		<div className="w-full h-full flex flex-col items-center justify-center gap-8 relative">
 			<div className="flex items-center gap-3">
 				<img
 					className=""
@@ -42,43 +108,16 @@ export function EnterCodeView() {
 					className="w-full"
 					onClick={() => {
 						clearQueryParams();
-						onSubmit("new");
+						dispatch(enterRoomCode("new"));
 					}}
 				>
 					Create room
 				</RaisedButton>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						onSubmit(e.currentTarget.roomCode.value);
-					}}
-					className="flex items-center gap-3 relative"
-				>
-					<div className="relative ">
-						<div className="flex items-center gap-3 bg-secondary-foreground rounded-lg">
-							<Input
-								name="roomCode"
-								autoComplete="off"
-								value={roomCode}
-								onChange={(e) => setRoomCode(e.target.value)}
-								placeholder="Room code"
-								className="font-bold text-xl text-zinc-400 placeholder:text-zinc-400 bg-background rounded-lg h-14 px-4 py-3.5 w-64 -translate-y-1.5 translate-x-1.5"
-							/>
-						</div>
-						<div className="absolute -right-14 top-2">
-							<RaisedButton
-								type="submit"
-								shift={false}
-								variant="action"
-								size="icon"
-							>
-								<ArrowRightIcon className="w-6 h-6" />
-							</RaisedButton>
-						</div>
-					</div>
-				</form>
+				<CodeForm />
 			</div>
+			{/* so that hills still appear on the left when spring overshoots */}
+			<Hills className="absolute bottom-0 right-full w-full" />
 			<Hills />
-		</motion.div>
+		</div>
 	);
 }
