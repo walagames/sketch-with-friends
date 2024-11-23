b8ab4a180f9712b2509acf21bcbd5a2a6a654a6f
Jacob <jacobschwantes@outlook.com>
2024-11-23 15:07:07 -0600

    update footer spacing

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -3,15 +3,14 @@ import { Button } from "./ui/button";
 
 export function Footer() {
 	return (
-		<footer className="absolute bottom-0 left-0 right-0 p-4 flex justify-between items-end">
+		<footer className="absolute bottom-0 left-0 right-0 px-3 py-2 flex justify-between items-end">
 			<p className="text-sm text-black flex items-center gap-1">
-				Built with ❤️ by the <img alt="" src="/walagames.png" className="h-6" />
+				Built with ❤️ by <img alt="" src="/walagames.png" className="h-6" />
 				<a href="https://github.com/walagames" target="_blank">
 					<Button size="sm" className="px-0" variant="link">
 						walagames
 					</Button>
 				</a>{" "}
-				team
 			</p>
 			<div className="flex">
 				<Link to="/terms-of-service">
