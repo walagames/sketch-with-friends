1569924e2d0b064e9458425dfc399cf34b511c3b
Jacob <jacobschwantes@outlook.com>
2024-12-25 16:10:33 -0600

    fix settings page overflow

--- frontend/src/components/views/pre-game/room-settings-form.tsx
+++ frontend/src/components/views/pre-game/room-settings-form.tsx
@@ -82,7 +82,7 @@ export function RoomSettingsForm() {
 	}, 300);
 
 	return (
-		<div className="w-full flex flex-col lg:gap-4 gap-1 max-h-[calc(100vh-200px)] overflow-y-auto px-1.5">
+		<div className="w-full flex flex-col lg:gap-4 gap-1 h-full px-1.5">
 			<h1 className="lg:text-3xl text-2xl font-bold z-10">Room settings</h1>
 			<Form {...form}>
 				<form
